ea829f51131f5b62681ed50a06cb900266383ae9
GutemaG <bir13gud17@gmail.com>
2022-12-08 08:06:40 +0300

    Change page layout

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
+import './style.css';
 import addTodo from './modules/addTodo.js';
 import displayTodos from './modules/displayTodos.js';
-import pageLayout from './modules/pageLayout.js';
-import './style.css';
 
-window.onload = pageLayout();
 displayTodos();
 addTodo();
