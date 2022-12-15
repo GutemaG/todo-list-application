0a8755a0b7fd4ecb307f8d4ad3814435f7506ca5
GutemaG <bir13gud17@gmail.com>
2022-12-15 22:51:02 +0300

    Add testcase for adding todo

---
+++ src/__test__/addTodo.test.js
@@ -0,0 +1,33 @@
+/*
+* @jest-environment jsdom
+*/
+import addTodo from '../modules/addTodo.js';
+
+describe('Testing add and remove', () => {
+  test('test adding todo', () => {
+    const description = 'todo-1';
+    const result = addTodo(description);
+    const dataInLocalStorage = [
+      {
+        description,
+        completed: false,
+        index: 1,
+      },
+    ];
+    expect(dataInLocalStorage).toEqual(result);
+    localStorage.setItem('todos', JSON.stringify(result));
+    expect(JSON.parse(localStorage.getItem('todos'))).toEqual(result);
+
+    document.body.innerHTML = '<div><ul id="todos"></ul></div>';
+    const todosContainer = document.querySelector('#todos');
+
+    for (let i = 0; i < result.length; i += 1) {
+      const todo = document.createElement('li');
+      todo.classList.add('todo-item');
+      todo.textContent = result[i].description;
+      todosContainer.appendChild(todo);
+    }
+    const list = document.querySelectorAll('.todo-item');
+    expect(list).toHaveLength(result.length);
+  });
+});
